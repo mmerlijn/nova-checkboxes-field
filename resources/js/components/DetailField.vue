73c00d86364280d3458dd6554e7e180fc00eb0f2
Menno <menno@mmerlijn.nl>
2023-01-17 23:06:09 +0100

    First version

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,17 @@
+<template>
+    <PanelItem :index="index" :field="field" >
+        <div class="text-sm grid grid-rows-4 grid-flow-col gap-2">
+            <div v-for="(label, option) in field.options" class="flex items-center">
+                <span :class="['rounded-full w-2 h-2 mr-1',(field.value ? field.value.includes(option):false) ? 'bg-green-600' : 'bg-red-600' ]"/>
+                <span>{{ label }}</span>
+            </div>
+        </div>
+    </PanelItem>
+</template>
+
+<script>
+export default {
+  props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+
+}
+</script>
---
+++ resources/js/components/FormField.vue
@@ -0,0 +1,75 @@
+<template>
+    <DefaultField
+        :field="field"
+        :errors="errors"
+        :show-help-text="showHelpText"
+        :full-width-content="fullWidthContent"
+    >
+        <template #field>
+            <div class="text-sm grid grid-rows-4 grid-flow-col gap-2">
+                <template v-for="(label, option) in field.options">
+                    <div class="flex items-center">
+                        <input
+                            type="checkbox"
+                            :value="option"
+                            :checked="isChecked(option)"
+                            @input="toggleOption(option)"
+                            class="checkbox mr-2"
+                            :id="field.name"
+                        />
+                        <label :for="field.name"
+                               v-text="label"
+                               @click="toggleOption(option)"
+                               class="w-full leading-tight"
+                        />
+                    </div>
+                </template>
+            </div>
+            <p v-if="hasError" class="my-2 text-danger">
+                {{ firstError }}
+            </p>
+        </template>
+    </DefaultField>
+</template>
+
+<script>
+import {FormField, HandlesValidationErrors} from 'laravel-nova'
+
+export default {
+    mixins: [FormField, HandlesValidationErrors],
+
+    props: ['resourceName', 'resourceId', 'field'],
+
+    methods: {
+        isChecked(option) {
+            return this.value ? this.value.includes(option) : false
+        },
+        toggleOption(option) {
+            if (this.isChecked(option)) {
+                this.$set(this, 'value', this.value.filter(item => item != option))
+                return
+            }
+            this.value.push(option)
+        },
+        /*
+         * Set the initial, internal value for the field.
+         */
+        setInitialValue() {
+            this.value = this.field.value || []
+        },
+
+        /**
+         * Fill the given FormData object with the field's internal value.
+         */
+        fill(formData) {
+            formData.append(this.field.attribute, this.value || [])
+        },
+        /**
+         * Update the field's internal value.
+         */
+        handleChange(value) {
+            this.value = value
+        }
+    },
+}
+</script>
